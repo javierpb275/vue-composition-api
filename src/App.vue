8e10c5d32cadd0e8c3eb8b77c2b7fc863477e743
javierpb275 <javierpb275@gmail.com>
2021-12-15 23:34:24 +0100

    created reactive variable in setup function using the ref function and updating its value with the function increment and displaying the reactive value on the page.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>hello</div>
+  <div>
+    <p>{{ num }}</p>
+    <button type="button" @click.prevent="increment">increment</button>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "App",
+  setup(props) {
+    let num = ref(0);
+    const increment = () => num.value++;
+    return {
+      num,
+      increment,
+    };
+  },
 };
 </script>
