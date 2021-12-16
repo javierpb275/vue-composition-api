e9ce474b15d14df8120b80c492ea3dcc90f1a7d3
javierpb275 <javierpb275@gmail.com>
2021-12-16 16:54:15 +0100

    used reactive() function for objects and torefs() function to access the properties of the object and keep them reactive.

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,33 @@
   <div>
     <p>{{ num }}</p>
     <button type="button" @click.prevent="increment">increment</button>
+    <p>{{name}}</p>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, reactive, toRefs } from "vue";
 
 export default {
   name: "App",
   setup(props) {
     let num = ref(0);
+
     const increment = () => num.value++;
+
+    const user = reactive({
+      name: 'Pepe',
+      age: 24
+    });
+
+    setTimeout(() => {
+      user.name = 'Paco'
+    }, 3000);
+
     return {
       num,
       increment,
+      ...toRefs(user)
     };
   },
 };
