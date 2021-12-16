a0501f9d19a50350bdf669ffb042de9c1b46533c
javierpb275 <javierpb275@gmail.com>
2021-12-16 17:44:37 +0100

    emot events in composition api

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -8,7 +8,8 @@ import { computed } from "vue";
 export default {
   name: "Alert",
   props: ["user"],
-  setup(props) {
+  setup(props, context) {
+    //context.emit('eventName', {})
     const flag = computed(() => props.user.name === "Pepe");
     return {
       flag,
