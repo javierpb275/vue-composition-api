f26eb1873e8bf74cbc7da2b8d9593784682f518d
javierpb275 <javierpb275@gmail.com>
2021-12-16 17:05:04 +0100

    watchers using composition api

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
     <p>{{ num }}</p>
     <button type="button" @click.prevent="increment">increment</button>
     <p>{{name}}</p>
+    <input type="text" v-model="phrase"/>
+    <p>{{reversedPhrase}}</p>
   </div>
 </template>
 
 <script>
-import { ref, reactive, toRefs } from "vue";
+import { ref, reactive, toRefs, watch } from "vue";
 
 export default {
   name: "App",
@@ -25,10 +27,23 @@ export default {
       user.name = 'Paco'
     }, 3000);
 
+    const phrase = ref("");
+    const reversedPhrase = ref("");
+
+    /* watch([phrase], ([newVal], [oldVal]) => {
+      reversedPhrase.value = phrase.value.split("").reverse().join("");
+    }); */
+
+    watch(() => {
+      reversedPhrase.value = phrase.value.split("").reverse().join("");
+    });
+
     return {
       num,
       increment,
-      ...toRefs(user)
+      ...toRefs(user),
+      phrase,
+      reversedPhrase
     };
   },
 };
